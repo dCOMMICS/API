c25cb04b482c4fad9247f88bea308bc22f11c1b9
dCOMMICS <sanwurld@gmail.com>
2023-10-19 00:51:04 +0300

    ❤✌

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,5 +2,9 @@ import mongoose from "mongoose";
 
 
 const UserSchema = new mongoose.Schema({
-    
-})
+   username: { type: 'string, required: true},
+    email: { type: 'string, required: true ,},
+    authenication:{
+        password: { type: 'string, required: true   select: false},
+    }
+});
