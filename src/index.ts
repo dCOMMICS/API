68a00f2864892871717fc8e54dab9bb31d401446
dCOMMICS <sanwurld@gmail.com>
2023-10-18 23:55:34 +0300

    🚀💪🏿

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log("WELCOME ERROR")
-console.log("welcome to the party")
+// console.log("WELCOME ERROR")
+// console.log("welcome to the party")
+
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
