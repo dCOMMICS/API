4914af2db92a5b61b7a1469214a53155fb756d83
dCOMMICS <sanwurld@gmail.com>
2023-10-19 17:57:03 +0300

    correction on naming done properly.

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 
 
 const UserSchema = new mongoose.Schema({
-   username: { type: 'string, required: true},
-    email: { type: 'string, required: true ,},
+    email: { type: String, required: true },
+   username: { type: String, required: true},
     authenication:{
-        password: { type: 'string, required: true   select: false},
-        salt: { type: 'string, selected: false},
-        sessionToken: { type: 'string, select: false}, 
+        password: { type: String, required: true,  select: false},
+        salt: { type: String, selected: false},
+        sessionToken: { type: String, select: false}, 
     },
 });
 
