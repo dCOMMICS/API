462fb694fd79394af3ec1e968f6f3c95b2c916ce
dCOMMICS <sanwurld@gmail.com>
2023-10-19 23:15:49 +0300

    💪🏿🔥

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { login, register } from '';
+import { login, register } from '../controller/authentication';
 
 export default (router: express.Router) => {
   router.post('/auth/register', register);
